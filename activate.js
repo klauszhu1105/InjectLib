41c55806334217fecf1e6597ae06661fcf5b8ee7
Klaus Zhu <38432931+klauszhu1105@users.noreply.github.com>
2023-09-30 23:47:19 +0800

    Update activate.js

--- activate.js
+++ activate.js
@@ -3,6 +3,7 @@ $done({
         body: JSON.stringify({
             success: true,
             response: {
+                product_id: "24534",
                 activation_id: "QiuChenly",
                 type: "personal",
                 expires: 1,
